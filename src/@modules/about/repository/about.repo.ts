cb8fd84d526ca0ec46e40776ec387ec543f2aa18
Radeoanres32 <bugra.linux.js@gmail.com>
2023-01-10 23:29:35 +0300

    abstract method delete added

--- src/@modules/about/repository/about.repo.ts
+++ src/@modules/about/repository/about.repo.ts
@@ -1,8 +1,8 @@
 import { IAbout } from '../entity/IAbout'
 export abstract class AboutRepository {
     abstract create():Promise<IAbout>
-    abstract find(name:string):Promise<IAbout>
+    abstract find(title:string):Promise<IAbout>
     abstract findAll():Promise<IAbout>
     abstract update(image?:string,title?:string,text?:string,description?:string):Promise<IAbout>
-
+    abstract delete(title?:string):Promise<IAbout>
 }
