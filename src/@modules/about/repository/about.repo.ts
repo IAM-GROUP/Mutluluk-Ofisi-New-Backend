df83a8289f87e4537de1df4c70b88e0b876af965
Radeoanres32 <bugra.linux.js@gmail.com>
2023-01-10 23:25:44 +0300

    abstract method findAll added

--- src/@modules/about/repository/about.repo.ts
+++ src/@modules/about/repository/about.repo.ts
@@ -2,5 +2,6 @@ import { IAbout } from '../entity/IAbout'
 export abstract class AboutRepository {
     abstract create():Promise<IAbout>
     abstract find(name:string):Promise<IAbout>
+    abstract findAll():Promise<IAbout>
 
 }
