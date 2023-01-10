b4c02f028d1863cb8fe466a832ed2ab77ffe3dd0
Radeoanres32 <bugra.linux.js@gmail.com>
2023-01-10 23:27:10 +0300

    abstract update method added

--- src/@modules/about/repository/about.repo.ts
+++ src/@modules/about/repository/about.repo.ts
@@ -3,5 +3,6 @@ export abstract class AboutRepository {
     abstract create():Promise<IAbout>
     abstract find(name:string):Promise<IAbout>
     abstract findAll():Promise<IAbout>
+    abstract update(image?:string,title?:string,text?:string,description?:string):Promise<IAbout>
 
 }
