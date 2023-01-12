d45f0099f0a43c92e3cdf50abb7afb145aeae2ea
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-13 00:51:21 +0300

    chalk exception use

--- src/core/data-source/mongo/connection.ts
+++ src/core/data-source/mongo/connection.ts
@@ -1,5 +1,5 @@
 import { connect } from 'mongoose'
-
+import chalk from 'chalk'
 //! Config
 import { Dotenv } from '../../config/config'
 
@@ -10,12 +10,6 @@ export const mongoConnection = async () => {
         await connect(`/mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?authSource=${process.env.MONGO_DB_NAME}`)
     }
     catch(exception) {
-
+        console.log(chalk.red('not connection ' + exception))
     }
 }
-
-
-
-
-
-//mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?authSource=${process.env.MONGO_DB_NAME}
