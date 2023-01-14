b112af870b3890e568856d95d7a36e08d23cce1b
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-14 23:16:57 +0300

    aboutFindAll service created

--- src/Api/@modules/about/services/about.service.ts
+++ src/Api/@modules/about/services/about.service.ts
@@ -2,7 +2,8 @@
 import { AboutDal } from '../dal/about.dal'
 import { IAbout } from '../entity/IAbout'
 
-export class AboutService implements AboutDal {
+export class AboutService {
+    private aboutDataAcess: AboutDal
     constructor(private id?: string, private image?: string, private title?: string, private text?: string, private description?: string, private html?: [{ title: string, context: string }], private icon?: [{ src: string, context: string }]) {
         this.id = id
         this.image = image
@@ -12,20 +13,8 @@ export class AboutService implements AboutDal {
         this.html = html
         this.icon = icon
     }
-    delete(id: string): Promise<{ message: string }> {
-        throw new Error('Method not implemented.')
-    }
-    create(): Promise<IAbout> {
-        throw new Error('Method not implemented.')
-    }
-    find(id: string): Promise<IAbout> {
-        throw new Error('Method not implemented.')
-    }
-    findAll(): Promise<IAbout[]> {
-        throw new Error('Method not implemented.')
-    }
-    update(id: string, image: string, title: string, text: string, description: string, html: [{ title: string; context: string }], icon: [{ src: string; context: string }]): Promise<{ message: string }> {
-        throw new Error('Method not implemented.')
+    get aboutFindAll(): Promise<IAbout[]> {
+        return this.aboutDataAcess.findAll()
     }
 
 }
