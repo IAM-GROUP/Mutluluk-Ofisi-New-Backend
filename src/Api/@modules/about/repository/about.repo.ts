429a49f63b4d50127e48969add5db6ba714f64c4
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-14 18:32:36 +0300

    repository updated

--- src/Api/@modules/about/repository/about.repo.ts
+++ src/Api/@modules/about/repository/about.repo.ts
@@ -1,7 +1,7 @@
 import { IAbout } from '../entity/IAbout'
 export interface AboutRepository {
     create(): Promise<IAbout>
-    find(title: string): Promise<IAbout>
+    find(title: string): Promise<string>
     findAll(): Promise<IAbout>
     update(image: string, title?: string, text?: string, description?: string): Promise<IAbout>
     delete(id: string): Promise<IAbout>
