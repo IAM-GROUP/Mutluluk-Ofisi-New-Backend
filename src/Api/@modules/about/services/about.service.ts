d047d80679d7cc4e0db9870c796f74b0505016b0
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-14 22:56:01 +0300

    service constructor created

---
+++ src/Api/@modules/about/services/about.service.ts
@@ -0,0 +1,11 @@
+export class AboutService {
+    constructor(private id?: string, private image?: string, private title?: string, private text?: string, private description?: string, private html?: [{ title: string, context: string }], private icon?: [{ src: string, context: string }]) {
+        this.id = id
+        this.image = image
+        this.title = title
+        this.description = description
+        this.text = text
+        this.html = html
+        this.icon = icon
+    }
+}
