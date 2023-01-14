3c23db5b3791ecf0822a6a5b5729082af42056f5
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-14 18:38:42 +0300

    update repo & delete dal added

--- src/Api/@modules/about/dal/about.dal.ts
+++ src/Api/@modules/about/dal/about.dal.ts
@@ -7,9 +7,16 @@ import { IAbout } from '../entity/IAbout'
 //? Models
 import { About } from '../models/about.models'
 export class AboutDal implements AboutRepository {
-    delete(id: string): Promise<IAbout> {
-        
-        return new Promise((resolve,reject)=>{})
+    async delete(id: string): Promise<{message:string}> {
+        await About.findByIdAndDelete(id)
+        return new Promise((resolve,reject)=>{
+            try {
+                return resolve({message:"Success deleted"})
+            }
+            catch(err) {
+                return reject({message:"Error " + err})
+            }
+        })
     }
     create():Promise<IAbout> {
         return new Promise((resolve,reject)=>{})
--- src/Api/@modules/about/repository/about.repo.ts
+++ src/Api/@modules/about/repository/about.repo.ts
@@ -1,8 +1,8 @@
 import { IAbout } from '../entity/IAbout'
 export interface AboutRepository {
     create(): Promise<IAbout>
-    find(title: string): Promise<string>
+    find(title: string): Promise<IAbout>
     findAll(): Promise<IAbout>
     update(image: string, title?: string, text?: string, description?: string): Promise<IAbout>
-    delete(id: string): Promise<IAbout>
+    delete(id: string): Promise<{message:string}>
 }
