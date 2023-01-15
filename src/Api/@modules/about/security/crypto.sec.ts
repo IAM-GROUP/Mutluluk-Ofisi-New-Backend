4dd1a8cac4df6a94ef3a7bcc9e05d65efed97d0d
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-15 16:30:37 +0300

    aes-256-cbc  serialize ecnrypt & decrypt added

---
+++ src/Api/@modules/about/security/crypto.sec.ts
@@ -0,0 +1,27 @@
+import { createCipheriv, createDecipheriv, randomBytes } from 'crypto'
+
+const key = randomBytes(32);
+const algorithm = 'aes-256-cbc';
+const iv = randomBytes(16);
+
+const encrypt = (text: any | any[]) => {
+    let cipher = createCipheriv(algorithm, Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return {
+        iv: iv.toString('hex'),
+        encryptedData: encrypted.toString('hex')
+    };
+}
+
+const decrypt = (text: any | any[]) => {
+    let iv = Buffer.from(text.iv, 'hex');
+    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+
+    let decipher = createDecipheriv(algorithm, Buffer.from(key), iv);
+
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+    return decrypted.toString();
+}
