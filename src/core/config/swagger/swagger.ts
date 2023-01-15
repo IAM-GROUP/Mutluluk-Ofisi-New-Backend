9a3ee87f3237cc7552cd92a79532e00bbe110b4f
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-16 00:52:25 +0300

    swagger basic conf

---
+++ src/core/config/swagger/swagger.ts
@@ -0,0 +1,19 @@
+import swaggerjsdoc from 'swagger-jsdoc'
+
+export const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Happy backend",
+            version: "1.0.0",
+            description: "Happy backend project"
+        },
+        servers: [
+            {
+                url: "http://127.0.0.1:3000"
+            }
+        ]
+    },
+    apis: [process.cwd() + 'src/Api/@modules/about/controllers/about.controller.ts']
+}
+export const swagger = swaggerjsdoc(options)
