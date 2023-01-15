283eb6859a88523b405afe8557277459c4db3863
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-16 01:01:26 +0300

    api doc layer created

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import http from 'http'
-
+import swaggerUI from 'swagger-ui-express'
 
 const app = express()
 const server = http.createServer(app)
@@ -12,7 +12,11 @@ import { mongoConnection } from './core/data-source/mongo/connection'
 //! Routes
 import { aboutMainRoutes } from './Api/@modules/about/routes'
 
+//! Swagger
+import { swagger } from './core/config/swagger/swagger'
+
 app.use('/', aboutMainRoutes)
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swagger))
 
 
 server.listen(3000, () => {
