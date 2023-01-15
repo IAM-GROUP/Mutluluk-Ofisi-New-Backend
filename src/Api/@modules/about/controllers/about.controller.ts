903fdda6488a5161722471e1933047af37f8d6c4
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-15 22:30:41 +0300

    getAbout service called

--- src/Api/@modules/about/controllers/about.controller.ts
+++ src/Api/@modules/about/controllers/about.controller.ts
@@ -3,6 +3,7 @@ import { Handler } from 'express'
 //! Service
 import { AboutService } from '../services/about.service'
 
-export const getAbout:Handler = (req,res) => {
-    
+export const getAbout: Handler = async (req, res) => {
+    const about = await new AboutService().aboutFindAll()
+    console.log(about)
 }
