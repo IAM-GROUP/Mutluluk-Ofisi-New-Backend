7cf7ec1c9fa2028459e384643a6119dca1a28c26
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-16 01:48:27 +0300

    getAbout return test json

--- src/Api/@modules/about/controllers/about.controller.ts
+++ src/Api/@modules/about/controllers/about.controller.ts
@@ -5,7 +5,7 @@ import { AboutService } from '../services/about.service'
 
 
 export const getAbout: Handler = async (req, res) => {
-    
+
     const about = await new AboutService().aboutFindAll()
-    console.log(about)
+    res.json({message:"test"})
 }
