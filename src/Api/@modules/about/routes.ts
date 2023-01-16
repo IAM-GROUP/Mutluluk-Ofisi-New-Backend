8d407d207ac4d65dae8a1955259235f8208d6bbe
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-16 23:43:26 +0300

    about main route added postAbout route

--- src/Api/@modules/about/routes.ts
+++ src/Api/@modules/about/routes.ts
@@ -2,6 +2,6 @@ import express from 'express'
 const app = express()
 
 //! About routes
-import { getAbout } from './routes/about.routes'
+import { getAbout,postAbout } from './routes/about.routes'
 
-export const  aboutMainRoutes = app.use('/api', getAbout) 
+export const  aboutMainRoutes = app.use('/api', getAbout,postAbout) 
