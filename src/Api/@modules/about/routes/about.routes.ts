a9c47cc2f75c58825f839c9663ea6aef3cf36f46
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-16 23:42:46 +0300

    post about route added

--- src/Api/@modules/about/routes/about.routes.ts
+++ src/Api/@modules/about/routes/about.routes.ts
@@ -4,5 +4,8 @@ const app = express.Router()
 //! Controller
 import { about  } from '../controllers/controllers'
 
-
+//? Get
 export const getAbout = app.get('/about-us',about.AboutController.getAbout)
+
+//* Post
+export const postAbout = app.post('/about-us',about.AboutController.createAbout)
