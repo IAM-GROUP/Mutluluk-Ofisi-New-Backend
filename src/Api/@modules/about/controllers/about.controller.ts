6ab8e1494fde514679f201e288b90cff6d26f699
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-19 22:51:55 +0300

    create about valdiation updated

--- src/Api/@modules/about/controllers/about.controller.ts
+++ src/Api/@modules/about/controllers/about.controller.ts
@@ -11,9 +11,9 @@ export class AboutController {
         res.json({ about })
     }
     static createAbout: Handler = async (req, res) => {
-        
+
         const aboutService = new AboutService()
-        const icons:[{src:string,context:string}] = [{src:"",context:""}] 
+        const icons: [{ src: string, context: string }] = [{ src: "", context: "" }]
         const { html, title, description, text, context } = req.body
 
         const { image, icon } = req.files as any | any[]
@@ -21,19 +21,28 @@ export class AboutController {
         icons.push({ src: icon[0].path, context })
         icons.shift()
         //test 
-        const encIcon:any = security.encrypt(icons)
-        const encHtml:any = security.encrypt(html)
-        if(encIcon.Error || encHtml.Error) {
+        const encIcon: any = security.encrypt(icons)
+        const encHtml: any = security.encrypt(html)
+        if (encIcon.Error || encHtml.Error) {
             res.json({
-                error:encIcon.Error || encHtml.Error
+                error: encIcon.Error || encHtml.Error
             })
         }
         else {
-            const about =  await aboutService.aboutCreate(image[0].path,title,text,description,encHtml,encIcon)
-            console.log(about)
+            const about = await aboutService.aboutCreate(image[0].path, title, text, description, encHtml, encIcon)
+            if (about.message) {
+                res.json({
+                    error: about.message
+                })
+            }
+            else {
+                res.json({
+                    message: (await about.create)?.message
+                })
+            }
         }
-        
-        
+
+
 
 
 
