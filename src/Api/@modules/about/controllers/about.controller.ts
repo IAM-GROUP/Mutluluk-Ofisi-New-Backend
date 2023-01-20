602e370354ccc5350c96e06cd76555eb6237fc77
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-21 00:44:02 +0300

    put about us test passed

--- src/Api/@modules/about/controllers/about.controller.ts
+++ src/Api/@modules/about/controllers/about.controller.ts
@@ -21,14 +21,14 @@ export class AboutController {
         icons.shift()
         //test 
         const encIcon: any = security.encrypt(icons)
-        const encHtml: any = security.encrypt(html)
-        if (encIcon.Error || encHtml.Error) {
+        /* const encHtml: any = security.encrypt(html) */
+        if (encIcon.Error || html.Error) {
             res.json({
-                error: encIcon.Error || encHtml.Error
+                error: encIcon.Error || html.Error
             })
         }
         else {
-            const about = await aboutService.aboutCreate(image[0].path, title, text, description, encHtml, encIcon)
+            const about = await aboutService.aboutCreate(image[0].path, title, text, description, html, encIcon)
             if (about.message) {
                 res.json({
                     error: about.message
--- src/Api/@modules/about/tests/about.test.ts
+++ src/Api/@modules/about/tests/about.test.ts
@@ -40,7 +40,21 @@ describe('About Us Api Testing...', () => {
             .on('error',(err)=>{
                 throw err
             })
-           
-      
     })
+    it('Put About Us',()=>{
+        request(server)
+        .put('/api/about-us')
+        .field('html','{"iv":"cfad375ed3dffe0c765d325f8f47b1a5","encryptedData":"dba036ffc3094bd118a5264ee376eb4c14a1698886d026581e5d5855abd61c8937be76467fb544532eabd5664c5f5196"}')
+            .field('title','test')
+            .field('id','63c9947cd0e4d88f091f656f')
+            .field('description','description')
+            .field('text','text')
+            .field('context','context') 
+            .attach('image',testImagePath)
+            .attach('icon',IconImagePath)
+            .on('error',(err)=>{
+                throw err
+            })
+    })
+
 })
