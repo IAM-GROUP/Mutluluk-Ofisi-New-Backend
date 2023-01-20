a928fb6f4fc690791d3bfee0477fc8e431e2c03d
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-21 00:45:16 +0300

    delete about us test passed

--- src/Api/@modules/about/tests/about.test.ts
+++ src/Api/@modules/about/tests/about.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect, it, beforeEach,afterEach } from '@jest/globals'
+import { describe, test, expect, it, beforeEach, afterEach } from '@jest/globals'
 import request from 'supertest'
 
 //!
@@ -30,29 +30,37 @@ describe('About Us Api Testing...', () => {
     it('Post About Us', () => {
         request(server)
             .post('/api/about-us')
-           .field('html','{"iv":"cfad375ed3dffe0c765d325f8f47b1a5","encryptedData":"dba036ffc3094bd118a5264ee376eb4c14a1698886d026581e5d5855abd61c8937be76467fb544532eabd5664c5f5196"}')
-            .field('title','test')
-            .field('description','description')
-            .field('text','text')
-            .field('context','context') 
-            .attach('image',testImagePath)
-            .attach('icon',IconImagePath)
-            .on('error',(err)=>{
+            .field('html', '{"iv":"cfad375ed3dffe0c765d325f8f47b1a5","encryptedData":"dba036ffc3094bd118a5264ee376eb4c14a1698886d026581e5d5855abd61c8937be76467fb544532eabd5664c5f5196"}')
+            .field('title', 'test')
+            .field('description', 'description')
+            .field('text', 'text')
+            .field('context', 'context')
+            .attach('image', testImagePath)
+            .attach('icon', IconImagePath)
+            .on('error', (err) => {
                 throw err
             })
     })
-    it('Put About Us',()=>{
+    it('Put About Us', () => {
         request(server)
-        .put('/api/about-us')
-        .field('html','{"iv":"cfad375ed3dffe0c765d325f8f47b1a5","encryptedData":"dba036ffc3094bd118a5264ee376eb4c14a1698886d026581e5d5855abd61c8937be76467fb544532eabd5664c5f5196"}')
-            .field('title','test')
-            .field('id','63c9947cd0e4d88f091f656f')
-            .field('description','description')
-            .field('text','text')
-            .field('context','context') 
-            .attach('image',testImagePath)
-            .attach('icon',IconImagePath)
-            .on('error',(err)=>{
+            .put('/api/about-us')
+            .field('html', '{"iv":"cfad375ed3dffe0c765d325f8f47b1a5","encryptedData":"dba036ffc3094bd118a5264ee376eb4c14a1698886d026581e5d5855abd61c8937be76467fb544532eabd5664c5f5196"}')
+            .field('title', 'test')
+            .field('id', '63c9947cd0e4d88f091f656f')
+            .field('description', 'description')
+            .field('text', 'text')
+            .field('context', 'context')
+            .attach('image', testImagePath)
+            .attach('icon', IconImagePath)
+            .on('error', (err) => {
+                throw err
+            })
+    })
+    it('Delete About Us', () => {
+        request(server)
+            .delete('/api/about-us')
+            .field('id', '63c9947cd0e4d88f091f656f')
+            .on('error', (err) => {
                 throw err
             })
     })
