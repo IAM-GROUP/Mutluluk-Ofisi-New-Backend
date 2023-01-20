55e208130bc5640bb7905e52bf51ec3ff33eeddb
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-20 21:55:42 +0300

    ts-jest package manager added for typescript used

--- src/Api/@modules/about/tests/about.test.ts
+++ src/Api/@modules/about/tests/about.test.ts
@@ -1,1 +1,19 @@
-import { describe,test,expect } from '@jest/globals'
+import { describe,test,expect,it } from '@jest/globals'
+import  request from 'supertest'
+
+
+//!
+import { server } from '../../../../server'
+
+describe('About Us Api Testing...',()=>{
+    it('Get All About Context',()=>{
+        request(server)
+        .get('/api/about-us')
+        .expect('Content-Type',/json/)
+        .expect(200)
+        .end((err,res)=>{
+            if(err) throw err
+            
+        })
+    })
+})
