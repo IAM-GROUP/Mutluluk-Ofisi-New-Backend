f9982da7e8cb84aa032f7a542038713d1aa24646
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-21 11:38:24 +0300

    about find id route added

--- src/Api/@modules/about/routes/about.routes.ts
+++ src/Api/@modules/about/routes/about.routes.ts
@@ -9,6 +9,7 @@ import { about } from '../controllers/controllers'
 
 //? Get
 export const getAbout = app.get('/about-us', about.AboutController.getAbout)
+export const getAboutId = app.get('/about-us/id',about.AboutController.getAboutId)
 
 //* Post
 export const postAbout = app.post('/about-us', middleware.multiuploads, about.AboutController.createAbout)
