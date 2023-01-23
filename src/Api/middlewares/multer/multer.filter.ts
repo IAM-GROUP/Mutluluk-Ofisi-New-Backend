53da9339a2d489af3138cea30eb31bd1b6044076
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-23 22:45:47 +0300

    multer upload filter module struct created

---
+++ src/Api/middlewares/multer/multer.filter.ts
@@ -0,0 +1,22 @@
+import { FileFilterCallback } from 'multer'
+import { Request, Express } from 'express'
+
+
+export function aboutUploadFilter({ _, file, cb }: {_:Request,file:Express.Multer.File,cb:FileFilterCallback}) {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    }
+    else {
+        cb(null, false);
+        return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+    }
+}
+export function aboutMainUploadFilter({ _, file, cb }: { _: Request; file: Express.Multer.File; cb: FileFilterCallback; }) {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    }
+    else {
+        cb(null, false);
+        return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+    }
+}
