f81833e6b554896098e66f2c99343465e3df8b7f
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-23 22:58:01 +0300

    multer func name fixed

--- src/Api/@modules/about/routes/about.routes.ts
+++ src/Api/@modules/about/routes/about.routes.ts
@@ -12,10 +12,10 @@ export const getAbout = app.get('/about-us', about.AboutController.getAbout)
 export const getAboutId = app.get('/about-us/id',about.AboutController.getAboutId)
 
 //* Post
-export const postAbout = app.post('/about-us', middleware.multiuploads, about.AboutController.createAbout)
+export const postAbout = app.post('/about-us', middleware.aboutMultiUploads, about.AboutController.createAbout)
 
 //? Update
-export const putAbout = app.put('/about-us',middleware.multiuploads,about.AboutController.updateAbout)
+export const putAbout = app.put('/about-us',middleware.aboutMultiUploads,about.AboutController.updateAbout)
 
 //! Delete
 export const deleteAbout = app.delete('/about-us',about.AboutController.deleteAbout)
