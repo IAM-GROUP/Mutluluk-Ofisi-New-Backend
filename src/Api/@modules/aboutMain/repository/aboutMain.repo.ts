2e02173d6bc4fb4e848e7a05440421fa549a3a93
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-23 20:54:45 +0300

    AboutRepository created

---
+++ src/Api/@modules/aboutMain/repository/aboutMain.repo.ts
@@ -0,0 +1,8 @@
+import { IAboutMain } from '../entity/IaboutMain'
+export interface  AboutRepository  {
+     create(image:string,title: string,html:[{ title: string; context: string; }]): Promise<{ message: string }>
+     find(id: string): Promise<IAboutMain>
+     findAll(): Promise<IAboutMain[]>
+     update(image:string,title: string,html:[{ title: string; context: string; }]): Promise<{ message: string }>
+     delete(id: string): Promise<{ message: string }>
+}
