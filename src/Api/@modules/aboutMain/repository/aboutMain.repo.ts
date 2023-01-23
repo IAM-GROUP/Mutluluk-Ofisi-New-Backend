e6ce312a7e0fc166e6699b295c9d0a29e09d4ca7
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-23 21:11:10 +0300

    aboutMainRepository update method param prop move

--- src/Api/@modules/aboutMain/repository/aboutMain.repo.ts
+++ src/Api/@modules/aboutMain/repository/aboutMain.repo.ts
@@ -3,6 +3,6 @@ export interface  AboutMainRepository  {
      create(title: string,html:[{ title: string; context: string; }],image:string,): Promise<{ message: string }>
      find(id: string): Promise<IAboutMain>
      findAll(): Promise<IAboutMain[]>
-     update(image:string,title: string,html:[{ title: string; context: string; }]): Promise<{ message: string }>
+     update(title: string,html:[{ title: string; context: string; }],image:string,): Promise<{ message: string }>
      delete(id: string): Promise<{ message: string }>
 }
