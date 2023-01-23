660c9073ef93fcaa6e256d72513a1083496fec92
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-23 21:12:12 +0300

    aboutRepository update method param add id

--- src/Api/@modules/aboutMain/repository/aboutMain.repo.ts
+++ src/Api/@modules/aboutMain/repository/aboutMain.repo.ts
@@ -3,6 +3,6 @@ export interface  AboutMainRepository  {
      create(title: string,html:[{ title: string; context: string; }],image:string,): Promise<{ message: string }>
      find(id: string): Promise<IAboutMain>
      findAll(): Promise<IAboutMain[]>
-     update(title: string,html:[{ title: string; context: string; }],image:string,): Promise<{ message: string }>
+     update(id:string,title: string,html:[{ title: string; context: string; }],image:string,): Promise<{ message: string }>
      delete(id: string): Promise<{ message: string }>
 }
