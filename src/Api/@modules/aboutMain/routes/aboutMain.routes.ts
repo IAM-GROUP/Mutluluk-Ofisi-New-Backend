11fe7efecbf4b11189e673d89450213460a294a0
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-23 23:10:58 +0300

    about main varr name changed

--- src/Api/@modules/aboutMain/routes/aboutMain.routes.ts
+++ src/Api/@modules/aboutMain/routes/aboutMain.routes.ts
@@ -8,14 +8,14 @@ import { middleware } from '../../../middlewares/middlewares'
 import { aboutMain } from '../controllers/controllers'
 
 //? Get
-export const getAbout = app.get('/about', aboutMain.AboutMainController.getAboutMain)
-export const getAboutId = app.get('/about/id',aboutMain.AboutMainController.getAboutMainId)
+export const getAboutMain = app.get('/about', aboutMain.AboutMainController.getAboutMain)
+export const getAboutMainId = app.get('/about/id',aboutMain.AboutMainController.getAboutMainId)
 
 //* Post
-export const postAbout = app.post('/about', middleware.aboutMainMultiUploads, aboutMain.AboutMainController.createAboutMain)
+export const postAboutMain = app.post('/about', middleware.aboutMainMultiUploads, aboutMain.AboutMainController.createAboutMain)
 
 //? Update
-export const putAbout = app.put('/about',middleware.aboutMainMultiUploads,aboutMain.AboutMainController.updateAboutMain)
+export const putAboutMain = app.put('/about',middleware.aboutMainMultiUploads,aboutMain.AboutMainController.updateAboutMain)
 
 //! Delete
-export const deleteAbout = app.delete('/about',aboutMain.AboutMainController.deleteAboutMain)
+export const deleteAboutMain = app.delete('/about',aboutMain.AboutMainController.deleteAboutMain)
