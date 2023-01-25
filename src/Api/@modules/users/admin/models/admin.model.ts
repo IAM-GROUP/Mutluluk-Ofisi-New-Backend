1b28a7c88a71d4b847ae5e6aa5779aeb21cac01a
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-25 18:23:01 +0300

    admin model created

---
+++ src/Api/@modules/users/admin/models/admin.model.ts
@@ -0,0 +1,14 @@
+import { model, Schema } from 'mongoose'
+
+//! Entity
+import { IAdmin } from '../entity/IAdmin'
+
+const AdminSchema = new Schema<IAdmin>({
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    }
+})
+export const Admin = model('About', AdminSchema)
