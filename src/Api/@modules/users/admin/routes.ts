0eea4ea869a79bf717eb6ef4fff1af9b37b08a19
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-25 19:09:57 +0300

    admin route created

---
+++ src/Api/@modules/users/admin/routes.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+const app = express()
+
+//! Admin routes
+import { getAdmin, deleteAdmin, getAdminId, postAdmin, putAdmin } from './routes/admin.routes'
+
+export const adminRoutes = app.use('/api', getAdmin, deleteAdmin, getAdminId, postAdmin, putAdmin)
---
+++ src/Api/@modules/users/admin/routes/admin.routes.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+const app = express.Router()
+
+//! Middleware
+import { middleware } from '../../../../middlewares/middlewares'
+
+//! Controller
+import { admin } from '../controllers/controllers'
+
+//? Get
+export const getAdmin = app.get('/admin', admin.AdminController.getAdmin)
+export const getAdminId = app.get('/admin/id', admin.AdminController.getAdminId)
+
+//* Post
+export const postAdmin = app.post('/admin', admin.AdminController.createAdmin)
+
+//? Update
+export const putAdmin = app.put('/admin', admin.AdminController.updateAdmin)
+
+//! Delete
+export const deleteAdmin = app.delete('/admin', admin.AdminController.deleteAdmin)
