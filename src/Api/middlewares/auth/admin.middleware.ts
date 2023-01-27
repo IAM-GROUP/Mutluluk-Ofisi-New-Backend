fd6e88f5a3f13f1a870778c3266867b2ef4630dd
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-27 16:43:09 +0300

    adminAuth method test

--- src/Api/middlewares/auth/admin.middleware.ts
+++ src/Api/middlewares/auth/admin.middleware.ts
@@ -1,5 +1,14 @@
 import { Handler } from 'express'
 
-export const adminAuth: Handler = (req, res, next) => {
-    
+//! Security
+import { security } from '../../security/security'
+
+export const adminAuth: Handler = ({ headers }, res, next) => {
+    const verify = security.jwt.token.verifyToken(headers['x-access-token'] as string)
+    if (verify.message) {
+        return res.status(verify.status as number).json(verify.message)
+    }
+    else {
+        console.log(verify.token)
+    }
 }
