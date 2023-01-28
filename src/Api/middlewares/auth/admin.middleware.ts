2b7ab4e5445ae9a26316b2afba462e1f8df5fd64
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-28 15:07:05 +0300

    admin auth method validation added

--- src/Api/middlewares/auth/admin.middleware.ts
+++ src/Api/middlewares/auth/admin.middleware.ts
@@ -3,12 +3,42 @@ import { Handler } from 'express'
 //! Security
 import { security } from '../../security/security'
 
-export const adminAuth: Handler = ({ headers }, res, next) => {
-    const verify = security.jwt.token.verifyToken(headers['x-access-token'] as string)
-    if (verify.message) {
-        return res.status(verify.status as number).json(verify.message)
+//! Service
+import { AdminService } from '../../@modules/users/admin/services/admin.service'
+
+//! Cache
+import { cache } from '../../cache/cache'
+
+export const adminAuth: Handler = async ({ headers }, res, next) => {
+    const adminService = new AdminService()
+    const token = headers['x-access-token'] as string
+    if (token) {
+        const isRedisToken = cache.redis.Token.checkToken(token)
+        if ((await isRedisToken).status === "valid") {
+            const verify = security.jwt.token.verifyToken(token)
+            if (verify.message) {
+                res.status(verify.status as number).json(verify.message)
+            }
+            else {
+                const email = verify.token?.payload?.email
+                const isEmail = await adminService.adminFindEmail(email as string)
+                if (isEmail.email) {
+                    next()
+                }
+                else {
+                    res.status(401).json({ message: isEmail.message })
+                }
+
+            }
+        }
+        else {
+            res.status(401).json({ message: (await isRedisToken).message })
+        }
     }
     else {
-        console.log(verify.token?.payload?.email)
+        res.status(401).json({
+            message: "token not found"
+        })
     }
+
 }
