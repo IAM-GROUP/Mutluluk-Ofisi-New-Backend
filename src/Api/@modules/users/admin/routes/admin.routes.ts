769e8064a9d649c6ec271b11c4d35398bd662f9c
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-30 00:58:48 +0300

    admin logout route created

--- src/Api/@modules/users/admin/routes/admin.routes.ts
+++ src/Api/@modules/users/admin/routes/admin.routes.ts
@@ -13,7 +13,8 @@ export const getAdminId = app.get('/id', admin.AdminController.getAdminId)
 
 //* Post
 export const postAdmin = app.post('/create', admin.AdminController.createAdmin)
-export const signAdmin = app.post('/sign',admin.AdminController.signAdmin)
+export const signAdmin = app.post('/sign', admin.AdminController.signAdmin)
+export const logoutAdmin = app.post('/logout', admin.AdminController.logoutAdmin)
 
 //? Update
 export const putAdmin = app.put('/update', admin.AdminController.updateAdmin)
