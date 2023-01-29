884867c8f57c783af04e890c0cbbd44d73a9ce85
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-30 00:59:21 +0300

    logoutAdmin main routes exported

--- src/Api/@modules/users/admin/routes.ts
+++ src/Api/@modules/users/admin/routes.ts
@@ -2,6 +2,6 @@ import express from 'express'
 const app = express.Router()
 
 //! Admin routes
-import { getAdmin, deleteAdmin, getAdminId, postAdmin, putAdmin,signAdmin } from './routes/admin.routes'
+import { getAdmin, deleteAdmin, getAdminId, postAdmin, putAdmin,signAdmin,logoutAdmin } from './routes/admin.routes'
 
-export const adminRoutes = app.use('/admin', getAdmin, deleteAdmin, getAdminId, postAdmin, putAdmin,signAdmin)
+export const adminRoutes = app.use('/admin', getAdmin, deleteAdmin, getAdminId, postAdmin, putAdmin,signAdmin,logoutAdmin)
