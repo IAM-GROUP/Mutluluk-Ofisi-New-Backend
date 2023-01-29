ed3a69e2828633848255ac43f5a840ab172255ee
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-30 01:12:47 +0300

    aboutMain route auth middleware added

--- src/Api/@modules/aboutMain/routes/aboutMain.routes.ts
+++ src/Api/@modules/aboutMain/routes/aboutMain.routes.ts
@@ -9,13 +9,13 @@ import { aboutMain } from '../controllers/controllers'
 
 //? Get
 export const getAboutMain = app.get('/about', aboutMain.AboutMainController.getAboutMain)
-export const getAboutMainId = app.get('/about/id',aboutMain.AboutMainController.getAboutMainId)
+export const getAboutMainId = app.get('/about/id', aboutMain.AboutMainController.getAboutMainId)
 
 //* Post
-export const postAboutMain = app.post('/about', middleware.aboutMainMultiUploads, aboutMain.AboutMainController.createAboutMain)
+export const postAboutMain = app.post('/about', [middleware.multer.aboutMainMultiUploads, middleware.auth.adminAuth], aboutMain.AboutMainController.createAboutMain)
 
 //? Update
-export const putAboutMain = app.put('/about',middleware.aboutMainMultiUploads,aboutMain.AboutMainController.updateAboutMain)
+export const putAboutMain = app.put('/about', [middleware.multer.aboutMainMultiUploads, middleware.auth.adminAuth], aboutMain.AboutMainController.updateAboutMain)
 
 //! Delete
-export const deleteAboutMain = app.delete('/about',aboutMain.AboutMainController.deleteAboutMain)
+export const deleteAboutMain = app.delete('/about', middleware.auth.adminAuth, aboutMain.AboutMainController.deleteAboutMain)
