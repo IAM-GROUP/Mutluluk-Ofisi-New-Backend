2dea835712cefa1b5efdc340741988a2358aadb2
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-30 00:40:52 +0300

    logout inline method created repo

--- src/Api/@modules/users/admin/repository/admin.repo.ts
+++ src/Api/@modules/users/admin/repository/admin.repo.ts
@@ -2,9 +2,10 @@ import { IAdmin } from '../entity/IAdmin'
 export interface AdminRepository {
     create(email: string, password: string): Promise<{ message: string }>
     find(id: string): Promise<IAdmin>
-    findEmail(email:string):Promise<IAdmin>
+    findEmail(email: string): Promise<IAdmin>
     findAll(): Promise<IAdmin[]>
     update(id: string, email: string, password: string): Promise<{ message: string }>
     delete(id: string): Promise<{ message: string }>
     sign(email: string, password: string): Promise<IAdmin>
+    logout(): Promise<{ message: string }>
 }
