1438f96aa1949f3e24ed63cab471f37defbc825f
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-30 01:11:41 +0300

    about route admin auth middleware added

--- src/Api/@modules/about/routes/about.routes.ts
+++ src/Api/@modules/about/routes/about.routes.ts
@@ -2,20 +2,20 @@ import express from 'express'
 const app = express.Router()
 
 //! Middleware
-import { middleware,auth } from '../../../middlewares/middlewares'
+import { middleware } from '../../../middlewares/middlewares'
 
 //! Controller
 import { about } from '../controllers/controllers'
 
 //? Get
 export const getAbout = app.get('/about-us', about.AboutController.getAbout)
-export const getAboutId = app.get('/about-us/id',about.AboutController.getAboutId)
+export const getAboutId = app.get('/about-us/id', about.AboutController.getAboutId)
 
 //* Post
-export const postAbout = app.post('/about-us', [middleware.aboutMultiUploads,auth.adminAuth],about.AboutController.createAbout)
+export const postAbout = app.post('/about-us', [middleware.multer.aboutMultiUploads, middleware.auth.adminAuth], about.AboutController.createAbout)
 
 //? Update
-export const putAbout = app.put('/about-us',middleware.aboutMultiUploads,about.AboutController.updateAbout)
+export const putAbout = app.put('/about-us', [middleware.multer.aboutMultiUploads, middleware.auth.adminAuth], about.AboutController.updateAbout)
 
 //! Delete
-export const deleteAbout = app.delete('/about-us',about.AboutController.deleteAbout)
+export const deleteAbout = app.delete('/about-us', middleware.auth.adminAuth, about.AboutController.deleteAbout)
