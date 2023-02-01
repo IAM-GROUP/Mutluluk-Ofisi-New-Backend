481b76e5c2003a3b8fb97fcea37b250456a61ec3
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-01 23:39:21 +0300

    category repo updated

--- src/Api/@modules/products/category/repository/category.repo.ts
+++ src/Api/@modules/products/category/repository/category.repo.ts
@@ -1,8 +1,8 @@
 import { ICategory } from '../entity/ICategory'
 export interface CategoryRepository {
-    create(title: string): Promise<{ message: string }>
+    create(title: string, categoryMain: Pick<ICategory, "categoryMain">): Promise<{ message: string }>
     find(id: string): Promise<ICategory>
     findAll(): Promise<ICategory[]>
-    update(id: string, title: string): Promise<{ message: string }>
+    update(id: string, title: string, categoryMain: Pick<ICategory, "categoryMain">): Promise<{ message: string }>
     delete(id: string): Promise<{ message: string }>
 }
