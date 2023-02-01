fd66e7006e7f1aa218df6c9c01586dd682d8faaa
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-02 00:32:29 +0300

    product main route added category routes

--- src/Api/@modules/products/routes.ts
+++ src/Api/@modules/products/routes.ts
@@ -3,5 +3,6 @@ const app = express.Router()
 
 //! CategoryMain routes
 import { categoryMainRoutes } from './categoryMain/routes'
+import { categoryRoutes } from './category/routes'
 
-export const productsRoute = app.use('/api/products', categoryMainRoutes)
+export const productsRoute = app.use('/api/products', categoryMainRoutes,categoryRoutes)
