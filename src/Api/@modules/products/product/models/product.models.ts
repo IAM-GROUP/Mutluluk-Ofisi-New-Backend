59189e43394440cb5e23d83cffd89d015eaa354b
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-03 02:02:25 +0300

    product model created

---
+++ src/Api/@modules/products/product/models/product.models.ts
@@ -0,0 +1,33 @@
+import { model, Schema } from 'mongoose'
+
+//! Entity
+import { IProduct } from '../entity/IProduct'
+
+const ProductSchema = new Schema<IProduct>({
+    name: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    types: {
+        type: [{ type: String, context: String }]
+    },
+    quantity: {
+        type: Number
+    },
+    images: {
+        type: [{ src: String }]
+    },
+    cargo: {
+        type: [{ title: String, src: String }]
+    },
+    property: {
+        type: String
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: "Category"
+    }
+})
+export const About = model('Product', ProductSchema)
