fd1e3ac02427497071622aff45f836a15477ab37
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-03 03:16:48 +0300

    multer middleware product & cargo added

--- src/Api/middlewares/multer/multer.middleware.ts
+++ src/Api/middlewares/multer/multer.middleware.ts
@@ -1,6 +1,8 @@
-import { aboutUpload, aboutMainUpload } from './config/multer.upload'
+import { aboutUpload, aboutMainUpload, cargoUpload, productUpload } from './config/multer.upload'
 
 
 
 export const aboutMultiUploads = aboutUpload.fields([{ name: "image", maxCount: 4 }, { name: "icon", maxCount: 4 }])
 export const aboutMainMultiUploads = aboutMainUpload.fields([{ name: "image", maxCount: 1 }])
+export const productMultiUploads = productUpload.fields([{ name: "image", maxCount: 4 }])
+export const cargoMultiUploads = cargoUpload.fields([{ name: "image", maxCount: 4 }])
