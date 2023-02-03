3ba2bf5b46a975eebe24bd88ceb91a60c524d64a
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-03 03:24:21 +0300

    main product layer route added product routes

--- src/Api/@modules/products/routes.ts
+++ src/Api/@modules/products/routes.ts
@@ -4,5 +4,6 @@ const app = express.Router()
 //! CategoryMain routes
 import { categoryMainRoutes } from './categoryMain/routes'
 import { categoryRoutes } from './category/routes'
+import { productRoutes } from './product/routes'
 
-export const productsRoute = app.use('/api/products', categoryMainRoutes,categoryRoutes)
+export const productsRoute = app.use('/api/products', categoryMainRoutes,categoryRoutes,productRoutes)
