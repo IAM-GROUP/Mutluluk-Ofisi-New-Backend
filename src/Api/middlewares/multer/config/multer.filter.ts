eb7d7160ae1dacaded850de3c20564196018a7d2
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-03 03:13:14 +0300

    product & cargo multer filter func created

--- src/Api/middlewares/multer/config/multer.filter.ts
+++ src/Api/middlewares/multer/config/multer.filter.ts
@@ -20,3 +20,21 @@ export function aboutMainUploadFilter({ _, file, cb }: { _: Request; file: Expre
         return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
     }
 }
+export function productUploadFilter({ _, file, cb }: { _: Request; file: Express.Multer.File; cb: FileFilterCallback; }) {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    }
+    else {
+        cb(null, false);
+        return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+    }
+}
+export function cargoUploadFilter({ _, file, cb }: { _: Request; file: Express.Multer.File; cb: FileFilterCallback; }) {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    }
+    else {
+        cb(null, false);
+        return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+    }
+}
