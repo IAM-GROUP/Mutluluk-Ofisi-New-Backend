a511997ef0507c57a82c4cb0de5908ce8da051c1
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-03 03:15:28 +0300

    multer upload product & cargo func added

--- src/Api/middlewares/multer/config/multer.upload.ts
+++ src/Api/middlewares/multer/config/multer.upload.ts
@@ -1,17 +1,25 @@
 import multer from 'multer'
 
 //! Storage
-import { aboutStorage, aboutMainStorage } from '../config/multer.storage'
+import { aboutStorage, aboutMainStorage, cargoStorage, productStorage } from '../config/multer.storage'
 
 //! Filter
-import { aboutMainUploadFilter, aboutUploadFilter } from './multer.filter'
+import { aboutMainUploadFilter, aboutUploadFilter, cargoUploadFilter, productUploadFilter } from './multer.filter'
 
 export const aboutUpload = multer({
     storage: aboutStorage,
-    fileFilter: (_,file,cb) => aboutUploadFilter({_,file,cb})
+    fileFilter: (_, file, cb) => aboutUploadFilter({ _, file, cb })
 })
 
 export const aboutMainUpload = multer({
     storage: aboutMainStorage,
-    fileFilter:(_,file,cb) => aboutMainUploadFilter({_,file,cb})
+    fileFilter: (_, file, cb) => aboutMainUploadFilter({ _, file, cb })
+})
+export const productUpload = multer({
+    storage: productStorage,
+    fileFilter: (_, file, cb) => productUploadFilter({ _, file, cb })
+})
+export const cargoUpload = multer({
+    storage: cargoStorage,
+    fileFilter: (_, file, cb) => cargoUploadFilter({ _, file, cb })
 })
