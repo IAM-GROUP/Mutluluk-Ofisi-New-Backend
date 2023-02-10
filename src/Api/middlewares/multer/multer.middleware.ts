d0f55b788060bc2553f012b9f6e679da12a11475
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-10 20:50:44 +0300

    project consultant upload multer middleware added

--- src/Api/middlewares/multer/multer.middleware.ts
+++ src/Api/middlewares/multer/multer.middleware.ts
@@ -1,7 +1,8 @@
-import { aboutUpload, aboutMainUpload, productUpload } from './config/multer.upload'
+import { aboutUpload, aboutMainUpload, productUpload, projectConsultantUpload } from './config/multer.upload'
 
 
 
 export const aboutMultiUploads = aboutUpload.fields([{ name: "image", maxCount: 4 }, { name: "icon", maxCount: 4 }])
 export const aboutMainMultiUploads = aboutMainUpload.fields([{ name: "image", maxCount: 1 }])
 export const productMultiUploads = productUpload.fields([{ name: "image", maxCount: 4 }, { name: "cargo", maxCount: 4 }])
+export const projectConsultantUploads = projectConsultantUpload.fields([{ name: "image", maxCount: 1 }])
