189f51fae62f0bb1e59393fddd1edbd7a8ab1162
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-10 20:51:23 +0300

    project consultant multer middleware added

--- src/Api/middlewares/middlewares.ts
+++ src/Api/middlewares/middlewares.ts
@@ -1,11 +1,12 @@
-import { aboutMainMultiUploads, aboutMultiUploads, productMultiUploads } from './multer/multer.middleware'
+import { aboutMainMultiUploads, aboutMultiUploads, productMultiUploads,projectConsultantUploads } from './multer/multer.middleware'
 import { adminAuth } from './auth/admin.middleware'
 
 export const middleware = {
     multer: {
         aboutMainMultiUploads,
         aboutMultiUploads,
-        productMultiUploads
+        productMultiUploads,
+        projectConsultantUploads
     },
     auth: {
         adminAuth
