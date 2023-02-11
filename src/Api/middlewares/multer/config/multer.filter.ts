d34e153be7d1ea75b7d734e07d80ef31b4ec9e82
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-11 23:33:39 +0300

    multer filter announcement added

--- src/Api/middlewares/multer/config/multer.filter.ts
+++ src/Api/middlewares/multer/config/multer.filter.ts
@@ -38,4 +38,14 @@ export function projectConsultantUploadFilter({ _, file, cb }: { _: Request; fil
         return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
     }
 }
+export function announcementUploadFilter({ _, file, cb }: { _: Request; file: Express.Multer.File; cb: FileFilterCallback; }) {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    }
+    else {
+        cb(null, false);
+        return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+    }
+}
+
 
