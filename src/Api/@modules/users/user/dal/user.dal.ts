b29ffb1d6d4aa28a1e57f6c2e751c01e71843de2
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-20 22:54:05 +0300

    user get follow method added

--- src/Api/@modules/users/user/dal/user.dal.ts
+++ src/Api/@modules/users/user/dal/user.dal.ts
@@ -70,8 +70,8 @@ export class UserDal implements UserRepository {
     async follow(follow: string, followers:string): Promise<{ message: string }> {
         return new Promise(async (resolve, reject) => {
             try {
-                const user =await neo4j()?.writeCypher("match (f1:user {id:$follow}) match(f2:user {id:$following}) create(f1)-[follow:FOLLOW]->(f2) create (f2)-[followers:FOLLOWERS]->(f1) ", { follow, followers });
-                resolve(user as any)
+                const user =await neo4j()?.writeCypher("match (f1:user {id:$follow}) match(f2:user {id:$followers}) create(f1)-[follow:FOLLOW]->(f2) create (f2)-[followers:FOLLOWERS]->(f1) ", { follow, followers });
+                resolve({ message: "Success following" })
             }
             catch (err) {
                 reject({ message: "Error " + err })
@@ -81,7 +81,18 @@ export class UserDal implements UserRepository {
     async unFollow(follow: string, followers:string): Promise<{ message: string }> {
         return new Promise(async (resolve, reject) => {
             try {
-                const user =await neo4j()?.writeCypher("match(f1:user {id:$follow})-[follow:FOLLOW]->(f2:user {id:$following}) match(f2:user {id:$following})-[followers:FOLLOWERS]->(f1:user {id:$follow}) delete followers,follow", { follow, followers });
+                const user =await neo4j()?.writeCypher("match(f1:user {id:$follow})-[follow:FOLLOW]->(f2:user {id:$followers}) match(f2:user {id:$following})-[followers:FOLLOWERS]->(f1:user {id:$follow}) delete followers,follow", { follow, followers });
+                resolve({ message: "Success un follow" })
+            }
+            catch (err) {
+                reject({ message: "Error " + err })
+            }
+        })
+    }
+    async getFollow(id:string): Promise<{ message: string }> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const user =await neo4j()?.cypher("match (n:user {id:$id})-[followers:FOLLOWERS]->(n1:user) return n1.id,n1.name,n1.surname", { id: id })
                 resolve(user as any)
             }
             catch (err) {
