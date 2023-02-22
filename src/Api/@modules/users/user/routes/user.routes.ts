04a988c33d6c59f07df090fb3fd6716195f4c906
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-23 00:26:20 +0300

    user route multer middleware added

--- src/Api/@modules/users/user/routes/user.routes.ts
+++ src/Api/@modules/users/user/routes/user.routes.ts
@@ -12,12 +12,12 @@ export const getUser = app.get('/', user.UserController.getUser)
 export const getUserId = app.get('/id', user.UserController.getUserId)
 
 //* Post
-export const postUser = app.post('/create', user.UserController.createUser)
+export const postUser = app.post('/create', middleware.multer.userUploads, user.UserController.createUser)
 export const signUser = app.post('/sign', user.UserController.signUser)
 /* export const logoutUser = app.post('/logout', admin.AdminController.logoutAdmin) */
 
 //? Update
-export const putUser = app.put('/update', user.UserController.updateUser)
+export const putUser = app.put('/update', middleware.multer.userUploads, user.UserController.updateUser)
 
 //! Delete
 export const deleteUser = app.delete('/delete', user.UserController.deleteUser)
