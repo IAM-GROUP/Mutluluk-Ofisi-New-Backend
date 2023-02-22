c2b8c627bef16ef0e5105a3e0486582ee5529e52
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-23 00:06:01 +0300

    user routes added

---
+++ src/Api/@modules/users/user/routes/user.routes.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+const app = express.Router()
+
+//! Middleware
+import { middleware } from '../../../../middlewares/middlewares'
+
+//! Controller
+import { user } from '../controllers/controllers'
+
+//? Get
+export const getUser = app.get('/', user.UserController.getUser)
+export const getUserId = app.get('/id', user.UserController.getUserId)
+
+//* Post
+export const postUser = app.post('/create', user.UserController.createUser)
+export const signUser = app.post('/sign', user.UserController.signUser)
+/* export const logoutUser = app.post('/logout', admin.AdminController.logoutAdmin) */
+
+//? Update
+export const putUser = app.put('/update', user.UserController.updateUser)
+
+//! Delete
+export const deleteUser = app.delete('/delete', user.UserController.deleteUser)
