01f4c0162b347719585256646998d7251afd64a5
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-28 21:26:29 +0300

    passport config updated

--- src/core/config/passport/passport.ts
+++ src/core/config/passport/passport.ts
@@ -6,7 +6,7 @@ export const passportConfig = (passport: any) => {
             {
                 clientID: "453159304412-ojjsgjb70sivit5s2i3mipj1lin04tli.apps.googleusercontent.com",
                 clientSecret: "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",
-                callbackURL: "/api/user/googleAuth",
+                callbackURL: "api/users/user/googleAuth",
             },
             async (accessToken, refreshToken, profile, done) => {
                 return done(null, profile);
