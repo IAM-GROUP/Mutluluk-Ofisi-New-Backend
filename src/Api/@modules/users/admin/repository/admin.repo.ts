28d9e54bbc1fe83872acbd87586a6e1fa83b4659
Radeonares32 <bugra.linux.js@gmail.com>
2023-03-03 02:47:43 +0300

    updated admin repo

--- src/Api/@modules/users/admin/repository/admin.repo.ts
+++ src/Api/@modules/users/admin/repository/admin.repo.ts
@@ -1,10 +1,10 @@
 import { IAdmin } from '../entity/IAdmin'
 export interface AdminRepository {
-    create(email: string, password: string): Promise<{ message: string }>
+    create(name:string,surname:string,email: string, password: string,gsmNumber:string,identityNumber:string,city:string,country:string,zipCode:string): Promise<{ message: string }>
     find(id: string): Promise<IAdmin>
     findEmail(email: string): Promise<IAdmin>
     findAll(): Promise<IAdmin[]>
-    update(id: string, email: string, password: string): Promise<{ message: string }>
+    update(id: string, name:string,surname:string,email: string, password: string,gsmNumber:string,identityNumber:string,city:string,country:string,zipCode:string): Promise<{ message: string }>
     delete(id: string): Promise<{ message: string }>
     sign(email: string, password: string): Promise<IAdmin>
 }
