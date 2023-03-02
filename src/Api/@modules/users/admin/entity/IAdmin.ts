32655a52fa615a98c4ff8b7ae20d0f74ab305e81
Radeonares32 <bugra.linux.js@gmail.com>
2023-03-03 02:43:57 +0300

    admin models updated

--- src/Api/@modules/users/admin/entity/IAdmin.ts
+++ src/Api/@modules/users/admin/entity/IAdmin.ts
@@ -1,4 +1,9 @@
 export interface IAdmin {
     email: string,
     password: string
+    gsmNumber: string,
+    identityNumber: string,
+    city: string
+    country: string,
+    zipCode: string
 }
