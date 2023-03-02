4c20ce543d05b5ae1642aeab1164c8202baf8691
Radeonares32 <bugra.linux.js@gmail.com>
2023-03-02 22:47:43 +0300

    postOrder method main route added

--- src/Api/@modules/users/user/routes.ts
+++ src/Api/@modules/users/user/routes.ts
@@ -3,6 +3,6 @@ const app = express.Router()
 
 //! User routes
 
-import { deleteUser, getUser, getUserId, postUser, putUser, signUser, getUserGoogleAuth, logoutUser, getGoogleUserSign, followUser, unFollowUser, getFollowUser,getFollowersUser,postBasket,getBasketUser,getOrderUser } from './routes/user.routes'
+import { deleteUser, getUser, getUserId, postUser, putUser, signUser, getUserGoogleAuth, logoutUser, getGoogleUserSign, followUser, unFollowUser, getFollowUser, getFollowersUser, postBasket, getBasketUser, getOrderUser, postOrder } from './routes/user.routes'
 
-export const userRoutes = app.use('/user', getUser, getUserId, postUser, putUser, signUser, deleteUser, getUserGoogleAuth, logoutUser, getGoogleUserSign, followUser, unFollowUser, getFollowUser,getFollowersUser,postBasket,getBasketUser,getOrderUser)
+export const userRoutes = app.use('/user', getUser, getUserId, postUser, putUser, signUser, deleteUser, getUserGoogleAuth, logoutUser, getGoogleUserSign, followUser, unFollowUser, getFollowUser, getFollowersUser, postBasket, getBasketUser, getOrderUser, postOrder)
