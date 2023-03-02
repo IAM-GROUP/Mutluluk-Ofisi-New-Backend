ca779201e19e00ceb56459e9b4f5b1609ca3129e
Radeonares32 <bugra.linux.js@gmail.com>
2023-03-02 22:22:33 +0300

    basket  main route added

--- src/Api/@modules/users/user/routes.ts
+++ src/Api/@modules/users/user/routes.ts
@@ -3,6 +3,6 @@ const app = express.Router()
 
 //! User routes
 
-import { deleteUser, getUser, getUserId, postUser, putUser, signUser, getUserGoogleAuth, logoutUser, getGoogleUserSign, followUser, unFollowUser, getFollowUser,getFollowersUser } from './routes/user.routes'
+import { deleteUser, getUser, getUserId, postUser, putUser, signUser, getUserGoogleAuth, logoutUser, getGoogleUserSign, followUser, unFollowUser, getFollowUser,getFollowersUser,postBasket } from './routes/user.routes'
 
-export const userRoutes = app.use('/user', getUser, getUserId, postUser, putUser, signUser, deleteUser, getUserGoogleAuth, logoutUser, getGoogleUserSign, followUser, unFollowUser, getFollowUser,getFollowersUser)
+export const userRoutes = app.use('/user', getUser, getUserId, postUser, putUser, signUser, deleteUser, getUserGoogleAuth, logoutUser, getGoogleUserSign, followUser, unFollowUser, getFollowUser,getFollowersUser,postBasket)
