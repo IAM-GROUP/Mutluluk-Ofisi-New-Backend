e40556d96e7dea4ee3d55a227ce0141f8dd09568
Radeonares32 <bugra.linux.js@gmail.com>
2023-03-04 23:43:11 +0300

    chat route main routes added

--- src/Api/@modules/users/user/routes.ts
+++ src/Api/@modules/users/user/routes.ts
@@ -3,6 +3,6 @@ const app = express.Router()
 
 //! User routes
 
-import { deleteUser, getUser, getUserId, postUser, putUser, signUser, getUserGoogleAuth, logoutUser, getGoogleUserSign, followUser, unFollowUser, getFollowUser, getFollowersUser, postBasket, getBasketUser, getOrderUser, postOrder,deleteRelRoles,deleteRole,getRole,getRoleUser,getRoles,getUserRole,postAddRoles,postRelAddRoles,putRole,postPayment } from './routes/user.routes'
+import { deleteUser, getUser, getUserId, postUser, putUser, signUser, getUserGoogleAuth, logoutUser, getGoogleUserSign, followUser, unFollowUser, getFollowUser, getFollowersUser, postBasket, getBasketUser, getOrderUser, postOrder,deleteRelRoles,deleteRole,getRole,getRoleUser,getRoles,getUserRole,postAddRoles,postRelAddRoles,putRole,postPayment,postJoinRoom,postSendMessage } from './routes/user.routes'
 
-export const userRoutes = app.use('/user', getUser, getUserId, postUser, putUser, signUser, deleteUser, getUserGoogleAuth, logoutUser, getGoogleUserSign, followUser, unFollowUser, getFollowUser, getFollowersUser, postBasket, getBasketUser, getOrderUser, postOrder,deleteRelRoles,deleteRole,getRole,getRoleUser,getRoles,getUserRole,postAddRoles,postRelAddRoles,putRole,postPayment)
+export const userRoutes = app.use('/user', getUser, getUserId, postUser, putUser, signUser, deleteUser, getUserGoogleAuth, logoutUser, getGoogleUserSign, followUser, unFollowUser, getFollowUser, getFollowersUser, postBasket, getBasketUser, getOrderUser, postOrder,deleteRelRoles,deleteRole,getRole,getRoleUser,getRoles,getUserRole,postAddRoles,postRelAddRoles,putRole,postPayment,postJoinRoom,postSendMessage)
