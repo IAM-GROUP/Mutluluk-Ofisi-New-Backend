4d1e9b88de8c2ed8ca5ffd345f958ac23a093814
Radeonares32 <bugra.linux.js@gmail.com>
2023-03-04 22:50:47 +0300

    socket entity & models created

--- src/Api/@modules/users/user/models/chat.models.ts
+++ src/Api/@modules/users/user/models/chat.models.ts
@@ -2,7 +2,7 @@ import { neo4j } from '../../../../../core/data-source/neo4j/connection'
 import { IChat } from '../entity/IChat';
 
 
-export const chat = neo4j()?.model<IChat>('Chat', {
+export const Chat = neo4j()?.model<IChat>('Chat', {
     id: {
         primary: true,
         type: 'uuid',
