fcb9bd4de645a72b502ce9d418b565c0ac5da6fd
Radeonares32 <bugra.linux.js@gmail.com>
2023-03-06 02:09:22 +0300

    openEdu repo created

--- src/Api/@modules/openEdu/openEdu/entity/IOpenEdu.entity.ts
+++ src/Api/@modules/openEdu/openEdu/entity/IOpenEdu.entity.ts
@@ -2,7 +2,7 @@ import { ObjectId } from 'mongoose'
 export interface IOpenEdu {
     name: string
     description: string
-    thumbnail_image: string
+    thumbnailImage: string
     html: string
     date: string
     status: boolean
