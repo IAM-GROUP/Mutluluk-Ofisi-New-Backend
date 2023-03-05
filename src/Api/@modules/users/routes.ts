f8f654f818dde6376d614a6e6a029bd77fd961c4
Radeonares32 <bugra.linux.js@gmail.com>
2023-03-05 21:21:19 +0300

    institutionalRoutes user route added

--- src/Api/@modules/users/routes.ts
+++ src/Api/@modules/users/routes.ts
@@ -4,5 +4,6 @@ const app = express.Router()
 //! Admin routes
 import { adminRoutes } from './admin/routes'
 import { userRoutes } from './user/routes'
+import { institutionalRoutes } from './institutional/routes'
 
-export const  userRoute = app.use('/api/users', userRoutes,adminRoutes)
+export const  userRoute = app.use('/api/users', userRoutes,adminRoutes,institutionalRoutes)
